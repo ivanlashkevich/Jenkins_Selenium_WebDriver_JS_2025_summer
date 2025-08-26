4a36b2bef0a8e9e5a20a41e546a4558c18833d33
ivanlashkevich <96345564+ivanlashkevich@users.noreply.github.com>
2025-08-26 23:15:12 +0300

    chore(pipelinePipelineConfiguration.spec.js): replace findElement method with explicit waits

--- oldTests/pipelinePipelineConfiguration.spec.js
+++ oldTests/pipelinePipelineConfiguration.spec.js
@@ -31,9 +31,15 @@ describe('US_02.004 | Pipeline > Pipeline Configuration', () => {
         await driver.manage().deleteAllCookies();
         await login(driver);
         
-        await driver.findElement(By.css('#side-panel [href$="newJob"]')).click();
-        await driver.findElement(By.id('name')).sendKeys(project.name);
-        await driver.findElement(By.className('org_jenkinsci_plugins_workflow_job_WorkflowJob')).click();
+        const newItemMenuOption = await driver.wait(until.elementLocated(By.css('#side-panel [href$="newJob"]')), 5000);
+        await newItemMenuOption.click();
+
+        const itemNameField = await driver.wait(until.elementLocated(By.id('name')), 5000);
+        await itemNameField.sendKeys(project.userName);
+
+        const pipelineProjectType = await driver.wait(until.elementLocated(By.className('org_jenkinsci_plugins_workflow_job_WorkflowJob')), 5000);
+        await pipelineProjectType.click();
+
         const okButton = await driver.wait(until.elementLocated(By.id('ok-button')), 5000);
         await driver.wait(until.elementIsVisible(okButton), 5000);
         await driver.wait(until.elementIsEnabled(okButton), 5000);
@@ -50,7 +56,9 @@ describe('US_02.004 | Pipeline > Pipeline Configuration', () => {
 
     it('TC_02.004.01 | Verify a pipeline can be disabled/enabled with the help of Enable/Disable toggle', async () => {
 
-        await driver.findElement(By.className('jenkins-toggle-switch__label__checked-title')).click();
+        let enabledToggle = await driver.wait(until.elementLocated(By.className('jenkins-toggle-switch__label__checked-title')), 5000);
+        await enabledToggle.click();
+
         const disabledToggle = await driver.wait(until.elementLocated(By.className('jenkins-toggle-switch__label__unchecked-title')), 5000);
         await driver.wait(until.elementIsVisible(disabledToggle), 5000);
         expect(await disabledToggle.isDisplayed()).to.be.true;
@@ -64,29 +72,39 @@ describe('US_02.004 | Pipeline > Pipeline Configuration', () => {
         await driver.wait(until.elementIsVisible(disabledProjectWarning), 5000);
         expect(await disabledProjectWarning.getText()).to.include(pipelineConfiguration.disabledProject);
         expect(await disabledProjectWarning.isDisplayed()).to.be.true;
+
         const enableButton = await driver.wait(until.elementLocated(By.css('button[name="Submit"]')), 5000);
         await driver.wait(until.elementIsVisible(enableButton), 5000);
         await driver.wait(until.elementIsEnabled(enableButton), 5000);
         await enableButton.click();
         await driver.wait(until.stalenessOf(enableButton), 5000);
-        await driver.findElement(By.css('#side-panel [href$="configure"]')).click();
 
-        const enabledToggle = await driver.wait(until.elementLocated(By.className('jenkins-toggle-switch__label__checked-title')), 5000);
+        const configureMenuOption = await driver.wait(until.elementLocated(By.css('#side-panel [href$="configure"]')), 5000);
+        await configureMenuOption.click();
+
+        enabledToggle = await driver.wait(until.elementLocated(By.className('jenkins-toggle-switch__label__checked-title')), 5000);
         await driver.wait(until.elementIsVisible(enabledToggle), 5000);
         expect(await enabledToggle.isDisplayed()).to.be.true;
     });
 
     it('TC_02.004.02 | Verify the choice of writing the pipeline directly in Jenkins, using the editor', async () => {
 
-        await driver.findElement(By.css('button[data-section-id="pipeline"]')).click();
+        let pipelineButton = await driver.wait(until.elementLocated(By.css('button[data-section-id="pipeline"]')), 5000);
+        await pipelineButton.click();
+
         await new Select(driver.findElement(By.css('#pipeline ~ .jenkins-form-item .jenkins-select__input.dropdownList'))).selectByVisibleText('Pipeline script');
         await new Select(driver.findElement(By.css('.samples > select'))).selectByVisibleText('Scripted Pipeline');
+
         const saveButton = await driver.wait(until.elementLocated(By.css('button[name="Submit"]')), 5000);
         await driver.wait(until.elementIsVisible(saveButton), 5000);
         await driver.wait(until.elementIsEnabled(saveButton), 5000);
         await saveButton.click();
-        await driver.findElement(By.css('#side-panel [href$="configure"]')).click();
-        await driver.findElement(By.css('button[data-section-id="pipeline"]')).click();
+
+        const configureMenuOption = await driver.wait(until.elementLocated(By.css('#side-panel [href$="configure"]')), 5000);
+        await configureMenuOption.click();
+
+        pipelineButton = await driver.wait(until.elementLocated(By.css('button[data-section-id="pipeline"]')), 5000);
+        await pipelineButton.click();
 
         const option = await driver.findElement(By.css('#pipeline ~ .jenkins-form-item .jenkins-select__input.dropdownList option[value="0"]'));
         expect(await option.isSelected()).to.be.true;
@@ -94,16 +112,25 @@ describe('US_02.004 | Pipeline > Pipeline Configuration', () => {
 
     it('TC_02.004.03 | Verify the choice of linking the pipeline to a Jenkinsfile stored in source control', async () => {
 
-        await driver.findElement(By.css('button[data-section-id="pipeline"]')).click();
+        let pipelineButton = await driver.wait(until.elementLocated(By.css('button[data-section-id="pipeline"]')), 5000);
+        await pipelineButton.click();
+
         await new Select(driver.findElement(By.css('#pipeline ~ .jenkins-form-item .jenkins-select__input.dropdownList'))).selectByVisibleText('Pipeline script from SCM');
         await new Select(driver.findElement(By.css('.dropdownList-container .jenkins-form-item .jenkins-select__input.dropdownList'))).selectByVisibleText('Git');
-        await driver.findElement(By.css('input[name="_.url"]')).sendKeys(projectRepositoryURL);
+
+        const repositoryURLInputField = await driver.wait(until.elementLocated(By.css('input[name="_.url"]')), 5000);
+        await repositoryURLInputField.sendKeys(projectRepositoryURL);
+
         const saveButton = await driver.wait(until.elementLocated(By.css('button[name="Submit"]')), 5000);
         await driver.wait(until.elementIsVisible(saveButton), 5000);
         await driver.wait(until.elementIsEnabled(saveButton), 5000);
         await saveButton.click();
-        await driver.findElement(By.css('#side-panel [href$="configure"]')).click();
-        await driver.findElement(By.css('button[data-section-id="pipeline"]')).click();
+
+        const configureMenuOption = await driver.wait(until.elementLocated(By.css('#side-panel [href$="configure"]')), 5000);
+        await configureMenuOption.click();
+
+        pipelineButton = await driver.wait(until.elementLocated(By.css('button[data-section-id="pipeline"]')), 5000);
+        await pipelineButton.click();
 
         const option = await driver.findElement(By.css('#pipeline ~ .jenkins-form-item .jenkins-select__input.dropdownList option[value="1"]'));
         expect(await option.isSelected()).to.be.true;

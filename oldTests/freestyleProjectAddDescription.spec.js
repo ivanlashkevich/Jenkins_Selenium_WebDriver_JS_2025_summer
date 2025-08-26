f8028fb8145670ec7a275fabc7e19ac096bb5634
ivanlashkevich <96345564+ivanlashkevich@users.noreply.github.com>
2025-08-26 18:31:31 +0300

    chore(freestyleProjectAddDescription.spec.js): replace findElement method with explicit waits

--- oldTests/freestyleProjectAddDescription.spec.js
+++ oldTests/freestyleProjectAddDescription.spec.js
@@ -5,7 +5,6 @@ import { cleanData } from '../support/cleanData.js';
 import genData from '../fixtures/genData.js';
 import { login } from '../fixtures/helperFunctions.js';
 
-
 describe('US_01.001 | FreestyleProject > Add description', () => {
 
     let driver;
@@ -25,9 +24,16 @@ describe('US_01.001 | FreestyleProject > Add description', () => {
         project = genData.newProject();
         await driver.manage().deleteAllCookies();
         await login(driver);
-        await driver.findElement(By.css('#side-panel [href$="newJob"]')).click();
-        await driver.findElement(By.id('name')).sendKeys(project.name);
-        await driver.findElement(By.css('li[class$="FreeStyleProject"]')).click();
+
+        const newItemMenuOption = await driver.wait(until.elementLocated(By.css('#side-panel [href$="newJob"]')), 5000);
+        await newItemMenuOption.click();
+
+        const newItemNameField = await driver.wait(until.elementLocated(By.id('name')), 5000);
+        await newItemNameField.sendKeys(project.name);
+
+        const freestyleProjectType = await driver.wait(until.elementLocated(By.css('li[class$="FreeStyleProject"]')), 5000);
+        await freestyleProjectType.click();
+        
         const okButton = await driver.wait(until.elementLocated(By.id('ok-button')), 5000);
         await driver.wait(until.elementIsVisible(okButton), 5000);
         await driver.wait(until.elementIsEnabled(okButton), 5000);
@@ -44,13 +50,15 @@ describe('US_01.001 | FreestyleProject > Add description', () => {
 
     it('TC_01.001.01 | Verify the possibility to add a description when creating a project', async () => {
 
-        await driver.findElement(By.css('textarea[name="description"]')).sendKeys(project.description);
+        const descriptionInputField = await driver.wait(until.elementLocated(By.css('textarea[name="description"]')), 5000);
+        await descriptionInputField.sendKeys(project.description);
+
         const saveButton = await driver.wait(until.elementLocated(By.css('button[name="Submit"]')), 5000);
         await driver.wait(until.elementIsVisible(saveButton), 5000);
         await driver.wait(until.elementIsEnabled(saveButton), 5000);
         await saveButton.click();
         
-        const description = await driver.findElement(By.id('description'));
+        const description = await driver.wait(until.elementLocated(By.id('description')), 5000);
         expect(await description.getText()).to.be.equal(project.description);
         expect(await description.isDisplayed()).to.be.true;
     });
@@ -61,36 +69,51 @@ describe('US_01.001 | FreestyleProject > Add description', () => {
         await driver.wait(until.elementIsVisible(saveButton), 5000);
         await driver.wait(until.elementIsEnabled(saveButton), 5000);
         await saveButton.click();
-        await driver.findElement(By.css('#side-panel [href$="configure"]')).click();
-        await driver.findElement(By.css('textarea[name="description"]')).sendKeys(project.description);
+        await driver.wait(until.stalenessOf(saveButton), 5000);
+
+        const configureMenuOption = await driver.wait(until.elementLocated(By.css('#side-panel [href$="configure"]')), 5000);
+        await configureMenuOption.click();
+
+        const descriptionInputField = await driver.wait(until.elementLocated(By.css('textarea[name="description"]')), 5000);
+        await descriptionInputField.sendKeys(project.description);
 
         saveButton = await driver.wait(until.elementLocated(By.css('button[name="Submit"]')), 5000);
         await driver.wait(until.elementIsVisible(saveButton), 5000);
         await driver.wait(until.elementIsEnabled(saveButton), 5000);
         await saveButton.click();
 
-        const description = await driver.findElement(By.id('description'));
+        const description = await driver.wait(until.elementLocated(By.id('description')), 5000);
+        await driver.wait(until.elementIsVisible(description), 5000);
         expect(await description.getText()).to.be.equal(project.description);
         expect(await description.isDisplayed()).to.be.true;
     });
 
     it('TC_01.001.03 | Verify that an existing description is updated', async () => {
 
-        await driver.findElement(By.css('textarea[name="description"]')).sendKeys(project.description);
-        const saveButton = await driver.wait(until.elementLocated(By.css('button[name="Submit"]')), 5000);
+        const descriptionInputField = await driver.wait(until.elementLocated(By.css('textarea[name="description"]')), 5000);
+        await descriptionInputField.sendKeys(project.description);
+
+        let saveButton = await driver.wait(until.elementLocated(By.css('button[name="Submit"]')), 5000);
         await driver.wait(until.elementIsVisible(saveButton), 5000);
         await driver.wait(until.elementIsEnabled(saveButton), 5000);
         await saveButton.click();
+        await driver.wait(until.stalenessOf(saveButton), 5000);
 
         let description = await driver.wait(until.elementLocated(By.id('description')), 5000);
         await driver.wait(until.elementIsVisible(description), 5000);
         expect(await description.getText()).to.be.equal(project.description);
 
-        await driver.findElement(By.id('description-link')).click();
-        const editDescriptionArea = await driver.findElement(By.css('textarea[name="description"]'));
+        const editDescriptionLink = await driver.wait(until.elementLocated(By.id('description-link')), 5000);
+        await editDescriptionLink.click();
+
+        const editDescriptionArea = await driver.wait(until.elementLocated(By.css('textarea[name="description"]')), 5000);
         await editDescriptionArea.clear();
         await editDescriptionArea.sendKeys(project.newDescription);
-        await driver.findElement(By.css('button[name="Submit"]')).click();
+
+        saveButton = await driver.wait(until.elementLocated(By.css('button[name="Submit"]')), 5000);
+        await driver.wait(until.elementIsVisible(saveButton), 5000);
+        await driver.wait(until.elementIsEnabled(saveButton), 5000);
+        await saveButton.click();
         await driver.wait(until.stalenessOf(description), 5000);
 
         description = await driver.wait(until.elementLocated(By.id('description')), 5000);
